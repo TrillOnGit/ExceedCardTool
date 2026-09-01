5b27cbb9652d438ca61aa493b1e7a3167e49edfd
TrillOnGit <austinfunk99@yahoo.ca>
2026-09-01 14:21:05 -0700

    Text wrapping for action text, and guard + armor patch display properly now

--- exceed-card-tool/src/CardPreview.tsx
+++ exceed-card-tool/src/CardPreview.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Card } from "./CardEditor";
-import frameSrc from "./assets/emptycard2C_wAG_CB.png";
+import frameSrc from "./assets/trueemptycard.png";
+import armPatchSrc from "./assets/armorpatch.png";
+import grdPatchSrc from "./assets/guardpatch.png";
+import contBoostIconSrc from "./assets/contboosticon.png";
 
 const nameFont = "56px ShaXizor";
 const statFont = "72px MKXTitle";
+const textFont = "32px AlgrySansMed";
 
 interface CardPreviewProps {
   card: Card;
@@ -11,6 +15,10 @@ interface CardPreviewProps {
 
 export function CardPreview({ card }: CardPreviewProps) {
   const frame = useImage(frameSrc);
+  const armPatch = useImage(armPatchSrc);
+  const grdPatch = useImage(grdPatchSrc);
+  const contBoostIcon = useImage(contBoostIconSrc);
+
   useEffect(() => {
     const cardCanvas = document.getElementById(
       "card-preview",
@@ -26,9 +34,16 @@ export function CardPreview({ card }: CardPreviewProps) {
     // Draw the Frame
     ctx.drawImage(frame, 0, 0);
 
+    // Draw the Patches
+    if (armPatch && card.armor > 0) {
+      ctx.drawImage(armPatch, 22, 430);
+    }
+    if (grdPatch && card.guard > 0) {
+      ctx.drawImage(grdPatch, 22, 532);
+    }
     // Draw the Name Text
-    ctx.letterSpacing = "1px";
     ctx.textAlign = "left";
+    ctx.letterSpacing = "1px";
     ctx.font = nameFont;
     ctx.fillText(card.name, 95, 89);
 
@@ -39,8 +54,13 @@ export function CardPreview({ card }: CardPreviewProps) {
     ctx.fillText(`${rangeToText(card.range)}`, 125, 183);
     ctx.fillText(`${powToText(card.power)}`, 116, 293);
     ctx.fillText(`${powToText(card.speed)}`, 110, 400);
-    ctx.fillText(`${powToText(card.armor)}`, 100, 507);
-    ctx.fillText(`${powToText(card.guard)}`, 100, 614);
+    ctx.fillText(`${defStatToText(card.armor)}`, 100, 507);
+    ctx.fillText(`${defStatToText(card.guard)}`, 100, 614);
+
+    // Draw the Action Text
+    ctx.letterSpacing = "0px";
+    ctx.font = textFont;
+    drawStackedLines(ctx, card.actionText, 375, 737);
   }, [card, frame]);
 
   return (
@@ -69,6 +89,60 @@ const rangeToText = (range: [number | undefined, number | undefined]) => {
 
 const powToText = (power: number | undefined) => {
   return Number.isInteger(power) ? `${power}` : "N/A";
+};
+
+function defStatToText(defStat: number) {
+  return Number.isInteger(defStat) && defStat > 0 ? `${defStat}` : "";
+}
+
+const drawStackedLines = (
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  x: number,
+  y: number,
+) => {
+  const lines = wrapTextLines(ctx, text);
+
+  const lineHeight = 35;
+  for (let i = 0; i < lines.length; i++) {
+    const offset = (i - (lines.length - 1) / 2) * lineHeight;
+    ctx.fillText(lines[i], x, y + offset);
+  }
+};
+
+// Returns lines of text that are split so that they do not exceed the text box width,
+// dropping text that exceeds the maximum number of lines.
+const wrapTextLines = (ctx: CanvasRenderingContext2D, text: string) => {
+  const textBoxWidth = 550;
+  const maxLines = 5;
+  const words = text.split(" ");
+  const lines: string[] = [];
+
+  if (ctx.measureText(text).width < textBoxWidth) {
+    lines.push(text);
+    return lines;
+  }
+
+  let currentLine = "";
+
+  for (const word of words) {
+    const candidateLine = currentLine ? `${currentLine} ${word}` : word;
+
+    if (ctx.measureText(candidateLine).width < textBoxWidth) {
+      currentLine = candidateLine;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
+    }
+
+    if (lines.length === maxLines) break;
+  }
+
+  if (lines.length < maxLines && currentLine) {
+    lines.push(currentLine);
+  }
+
+  return lines;
 };
 
 const useImage = (src: string) => {
